adb16e6cc2b4f0a2c1fdc72e4de3173e405d4bc8
Tiryoh <tiryoh@gmail.com>
2019-02-23 23:41:11 +0900

    Initial commit

---
+++ order_lottery.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+PATH_TO_LIST = "list.txt"
+PATH_TO_RESULT = "result.txt"
+
+if __name__ == "__main__":
+    with open(PATH_TO_LIST, mode="r") as f:
+        names = [s.strip() for s in f.readlines()]
+    names = [x for x in names if x]
+    order = random.sample(names, len(names))
+    print(names)
+    print(order)
+    with open(PATH_TO_RESULT, mode="w") as f:
+        for name in order:
+            f.write(name+"\n")
